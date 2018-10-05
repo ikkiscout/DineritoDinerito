8279d66d41635308f64cd06cc9a43eddb6b77894
Oscar Arranz <theikkiscout@gmail.com>
2018-10-05 14:10:56 -0700

    Captura
    Hacer captura de pantalla funcionando correctamente

--- src/imagen/Captura.java
+++ src/imagen/Captura.java
@@ -1,5 +1,39 @@
 package imagen;
 
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+ 
+import javax.imageio.ImageIO;
+
 public class Captura {
+	
+	public static void main(String[] args) {
+		capturar(25, 585 , 519, 992);
+	}
+	
+	public static String capturar(int x1, int y1, int x2, int y2) {
+		try {
+            Robot robot = new Robot();
+            String format = "jpg";
+            String fileName = "scr." + format;
+             
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            Rectangle captureRect = new Rectangle(x1, y1, x2-x1, y2-y1);
+            BufferedImage screenFullImage = robot.createScreenCapture(captureRect);
+            ImageIO.write(screenFullImage, format, new File(fileName));
+             
+            System.out.println("¡Captura guardada!");
+            return fileName;
+        } catch (AWTException | IOException ex) {
+            System.err.println(ex);
+            return null;
+        }
+	}
 
 }
